61d7af22759347965041bcf1c9a3376a619e5388
wspark <jjomacson@gmail.com>
2022-05-21 23:53:06 +0900

    Update DemoApplication.java

--- src/main/java/com/example/nhn/demo/DemoApplication.java
+++ src/main/java/com/example/nhn/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	public String home() {
-	  return "Hello Docker World";
+	  return "Hello Docker World deploy by tekton";
 	}
 
 	public static void main(String[] args) {
