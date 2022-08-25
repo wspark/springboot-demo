a42c213cd37f64ae69112b2b6d5e56fc434e756a
박원석 <jjomacson@gmail.com>
2022-08-26 00:47:56 +0900

    rest template test

--- springboot-demo-book/src/main/java/com/example/rock/demo/DemoApplication.java
+++ springboot-demo-book/src/main/java/com/example/rock/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.rock.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
@@ -9,6 +11,11 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+	   return new RestTemplate();
 	}
 
 }
--- springboot-demo-book/src/main/java/com/example/rock/demo/controller/LibraryController.java
+++ springboot-demo-book/src/main/java/com/example/rock/demo/controller/LibraryController.java
@@ -19,6 +19,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+// resttemplate test
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+
 import java.util.List;
 
 @RestController
@@ -28,6 +36,20 @@ import java.util.List;
 public class LibraryController {
     
     private final LibraryService libraryService;
+
+    @Autowired
+    RestTemplate restTemplate;
+  //  String memberUrl = "http://springboot-demo-member-wspark-test.apps.ocp4.local/api/library/member/1";
+  String memberUrl = "http://springboot-demo-member.wspark-test.svc.cluster.local/api/library/member/1";
+
+    @GetMapping("/book/rest")
+    public String getMemberList(){
+         HttpHeaders headers = new HttpHeaders();
+       //  headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+         HttpEntity <String> entity = new HttpEntity<String>(headers);
+
+         return restTemplate.exchange(memberUrl, HttpMethod.GET, entity, String.class).getBody();      
+    }
 
     @Timed(value = "get-book")
     @GetMapping("/book")
