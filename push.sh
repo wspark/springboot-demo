bfe02ea8eab93f3df5664a853369c47e0ec95a8d
Your Name~ <you@example.com>
2023-01-09 11:16:31 +0900

    ok

--- push.sh
+++ push.sh
@@ -1,14 +1,16 @@
 
 #/bin/bash
 
+echo "book jar copy to deployments"
+cp ./springboot-demo-book/target/book-0.0.1-SNAPSHOT.jar ./springboot-demo-book/deployments/
+echo "member jar copy to deployments"
+cp ./springboot-demo-member/target/member-0.0.1-SNAPSHOT.jar ./springboot-demo-member/deployments/
+
+
 echo "git push start"
 git add .
 git commit -m "ok"
 git push
 echo "git push end"
 
-echo "book jar copy to deployments"
-cp ./springboot-demo-book/target/book-0.0.1-SNAPSHOT.jar ./springboot-demo-book/deployments/
-echo "member jar copy to deployments"
-cp ./springboot-demo-member/target/member-0.0.1-SNAPSHOT.jar ./springboot-demo-member/deployments/
 
